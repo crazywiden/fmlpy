0370a23393e2aebcf4f180a54953fb976828ed7f
crazywiden <36159733+crazywiden@users.noreply.github.com>
2019-03-06 21:39:47 -0600

    Update bars.py

--- preprocessing/bars.py
+++ preprocessing/bars.py
@@ -8,3 +8,9 @@ volume_imbalance_bar()
 tick_run_bar()
 volume_run_bar()
 """
+def _preprocess(data,T=None,price=None,vol=None):
+  """
+  this function is used to do the following things:
+  1. aggregate the data input as a dataframe
+  2. exception handling
+  """
