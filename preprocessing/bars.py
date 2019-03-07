8deb2b11f0aeef318a91368702e925fda80d5a91
crazywiden <36159733+crazywiden@users.noreply.github.com>
2019-03-07 12:48:40 -0600

    Update bars.py

--- preprocessing/bars.py
+++ preprocessing/bars.py
@@ -12,33 +12,39 @@ import pandas as pd
 from functools import reduce
 
 def _preprocess(data, need_vol=False):
-  """
-  this function is used to do the following things:
-  1. exception handling (determine if the data is ideal data)
-  2. aggregate the data input as a dataframe
-  Note that this function is a private function only for this module
-  @requires:
+    """
+    this function is used to do the following things:
+    1. exception handling (determine if the data is ideal data)
+    2. aggregate the data input as a dataframe
+    Note that this function is a private function only for this module
+    @requires:
     pandas
-  @parameters:
-  data--dataframe
-    default is None
-    the ideal data should have at least one columns: price
-    if only one column, then we need add another column T (use index of each row)
-    if two column, treat the first column as T
-    if need_vol is True, then must have three columns and treat the third column as Volume
-  need_vol--binary
-    default is False
-    when need_vol == False, the data variable can have two columns
-    when need_vol == True, the data variable should have at least three columns with the last column is volume
-  @returns:
+    @parameters:
+    data--dataframe
+        default is None
+        the ideal data should have at least one columns: price
+        if only one column, then we need add another column T (use index of each row)
+        if two column, treat the first column as T
+        if need_vol is True, then must have three columns and treat the third column as Volume
+        need_vol--binary
+        default is False
+        when need_vol == False, the data variable can have two columns
+        when need_vol == True, the data variable should have at least three columns with the last column is volume
+    @returns:
     data-- dataframe
-    with two or three columns
-    if two column, treat the first column as T
-    if need_vol is True, then must have three columns and treat the third column as Volume
-  """
-  if not isinstance(data,pd.DataFrame):
-    raise TypeError("the input should be DataFrame")
-  return data
+        with two or three columns
+        if two column, treat the first column as T
+        if need_vol is True, then must have three columns and treat the third column as Volume
+    """
+    if not isinstance(data,pd.DataFrame):
+        raise TypeError("the input should be DataFrame")
+    if not need_vol:
+        data = data.iloc[:,:2]
+        data.columns = ["time","price"]
+    else:
+        data = data.iloc[:,:3]
+        data.columns = ["time","price","vol"]
+    return data
 
 
 
@@ -55,8 +61,8 @@ def time_bar(data, time_window):
     '''
     data = _preprocess(data)
     time_window = pd.Timedelta(time_window)
-    merged = data.groupby('time').agg(['min', 'max', 'first', 'last']).reset_index()
-    start = end = pd.to_datetime(data.iloc[0, 'time'])
+    merged = data.groupby('time').agg(['min', 'max', 'first', 'last']).reset_index() #there might be something wrong here
+    start = end = pd.to_datetime(data['time'][0])
     lo = float("inf")
     hi = 0
     open = merged.iloc[0]['price']['first']
