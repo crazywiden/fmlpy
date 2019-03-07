663e5b28c46f2f35ad2f3964bd7b474e9a8a91b7
hxiuyu2 <beauniverse19@gmail.com>
2019-03-07 06:16:11 -0600

    20190306 meeting: add interface for some functions in bars.py

--- preprocessing/bars.py
+++ preprocessing/bars.py
@@ -8,3 +8,13 @@ volume_imbalance_bar()
 tick_run_bar()
 volume_run_bar()
 """
+
+def time_bar(data, time_window):
+    data = preproc(data)
+    
+    # return: dataframe (start, stop, HLOC)
+    # time win intergeer
+    # exception?
+
+def volume_bar(data, size):
+    # return dataframe
