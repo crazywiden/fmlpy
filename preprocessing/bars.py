2d99321089215f4b19108cb71e90d2f4738e86f7
crazywiden <36159733+crazywiden@users.noreply.github.com>
2019-03-06 23:08:53 -0600

    Update bars.py

--- preprocessing/bars.py
+++ preprocessing/bars.py
@@ -8,9 +8,32 @@ volume_imbalance_bar()
 tick_run_bar()
 volume_run_bar()
 """
-def _preprocess(data,T=None,price=None,vol=None):
+def _preprocess(data, need_vol=False):
   """
   this function is used to do the following things:
-  1. aggregate the data input as a dataframe
-  2. exception handling
+  1. exception handling (determine if the data is ideal data)
+  2. aggregate the data input as a dataframe
+  Note that this function is a private function only for this module
+  @requires:
+    pandas
+  @parameters:
+  data--dataframe
+    default is None
+    the ideal data should have at least one columns: price
+    if only one column, then we need add another column T (use index of each row)
+    if two column, treat the first column as T
+    if need_vol is True, then must have three columns and treat the third column as Volume
+  need_vol--binary
+    default is False
+    when need_vol == False, the data variable can have two columns
+    when need_vol == True, the data variable should have at least three columns with the last column is volume
+  @returns:
+    data-- dataframe
+    with two or three columns
+    if two column, treat the first column as T
+    if need_vol is True, then must have three columns and treat the third column as Volume
   """
+  if not isinstance(data,pd.DataFrame):
+    raise TypeError("the input should be DataFrame")
+  return data
+
