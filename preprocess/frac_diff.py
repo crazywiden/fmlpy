4effbc89b9fc170aded315c777bb13b643930664
hxiuyu2 <beauniverse19@gmail.com>
2019-03-25 14:48:34 -0500

    fraction diff, not sure about understanding

--- preprocess/frac_diff.py
+++ preprocess/frac_diff.py
@@ -2,17 +2,23 @@
 this script is used to calculate fractionally differentiated 
 and provide option to test the stationary of a series
 """
+import pandas as pd
+import numpy as np
+
 def frac_diff(n, x):
     '''
     This function is used to calculate fraction differenciation
     :param n: total number to iterate
-    :param k: don't know how to say in English
     :param x: kind of some number
     :return: some calculated result
     '''
-    comb_list = _combine(n)
-    answer = [a*b for a,b in zip(x,comb_list)]
-    return answer
+    length = len(x) - n
+    df = pd.DataFrame(columns=range(length))
+    for i in range(n+1):
+        df.loc[len(df), :] = x[i: i+length]
+    result = df.apply(func=_one_frac, base=n, axis=0).to_list()
+    res = x[:n] + result
+    return res
 
 
 def _combine(n):
@@ -21,10 +27,19 @@ def _combine(n):
     :param n:
     :return: a list of combination number
     '''
-    result = [None]*n
-    sign = 1 if n % 2 == 0 else -1
-    result[0] = 1*sign
+    result = [None] * (n+1)
+    sign = -1
+    result[0] = 1
     for i in range(1, n+1):
-        sign = -sign
         result[i] = (n-i+1)/i*result[i-1]*sign
     return result
+
+
+def _one_frac(x, base):
+    comb_list = _combine(base)
+    answer = [a * b for a, b in zip(x[::-1], comb_list)]
+    return np.sum(answer)
+
+
+res = frac_diff(2, [1,2,3,4,5,6,7,8,9,10])
+print(res)
