c54c6357eac63a99ff9cefdc6ee9cba51f561952
widen <widen1226@163.com>
2019-04-05 01:07:08 -0500

    modified test file of meta label function

--- tests/preprocess/gen_labelling.R
+++ tests/preprocess/gen_labelling.R
@@ -1,32 +1,21 @@
-library(lubridate)
 library(fmlr)
 library(optparse)
-library(anytime)
 rm(list=ls())
-options(digits.secs=3)
 
-preprocess <- function(raw_data){
-  # raw_data is a data.frame
-  names(raw_data) <- c("time", "Price","Size")
-  raw_data$time <- anytime(raw_data$time)
-  raw_data$Size <- as.numeric(raw_data$Size)
-  raw_data$Price <- as.numeric(raw_data$Price)
-  return(raw_data)
-}
 
 option_list = list(
   make_option(c("--pt"), type="double",default=1,
               help="profit taking", metavar="character"),
   make_option(c("--sl"), type="double", default=1,
               help="stop loss", metavar="character"),
-  make_option(c("--thres"), type="integer", default=1000,
+  make_option(c("--thres"), type="integer", default=200,
               help="threshold for CUMSUM filter", metavar="character"),
-  make_option(c("--target"), type="double", default=0.005,
+  make_option(c("--target"), type="double", default=0.001,
               help="minimum return of each label(we assume symmetric)", 
               metavar="character"),
   make_option(c("--hold"), type="integer", default=200,
               help="maximum holding time", metavar = "character"),
-  make_option(c("--is_vertical"), type="character", default="T",
+  make_option(c("--inclu_vertical"), type="character", default="T",
               help="whether consider vertical bar or not", metavar = "character")
 )
 
@@ -36,18 +25,18 @@ root_path <- getwd()
 # root_path <- "D://fmlpy//tests//preprocess"
 data_path <- paste(root_path,"bar_test_data.csv",sep = "/")
 raw_data <- read.csv(data_path,header = T)
-raw_data <- preprocess(raw_data)
+# raw_data <- preprocess(raw_data)
 
 thres <- opt$thres
 hold_T <- opt$hold
 target <- opt$target
 profit_taking <- opt$pt
 stop_loss <- opt$sl
-vert <- opt$is_vertical
-if(vert %in% c("T","True","TRUE")){
-  is_vert <- TRUE
-}else if(vert %in% c("F","False", "FALSE")){
-  is_vert <- FALSE
+in_vert <- opt$inclu_vertical
+if(in_vert %in% c("T","True","TRUE")){
+  ex_vert <- FALSE
+}else if(in_vert %in% c("F","False", "FALSE")){
+  ex_vert <- TRUE
 }else{
   stop("please enter proper value: T/True/TRUE/F/False/FALSE")
 }
@@ -60,9 +49,8 @@ n_event <- length(CUMSUM_idx)
 # # side[i] == 1 means only take profit taking 
 # # side[i] == -1 means only take stop loss
 events <- data.frame(t0=CUMSUM_idx+1, t1=CUMSUM_idx+hold_T, trgt=rep(target, n_event), side=rep(0, n_event))
-
 ptSl <- c(profit_taking, stop_loss)
-out <- fmlr::label_meta(price, events, ptSl, ex_vert = is_vert)
+out <- fmlr::label_meta(price, events, ptSl, ex_vert = ex_vert)
 write.csv(out,file=paste(root_path,"meta_label_run.csv",sep="/"))
 
 
--- tests/preprocess/labelling_test.py
+++ tests/preprocess/labelling_test.py
@@ -22,8 +22,8 @@ def parser_args():
         help="minimum return of each label(we assume symmetric)")
     parser.add_argument("--hold", type=int, default=200,\
         help="maximum holding time")
-    parser.add_argument("--is_vertical", default="T",\
-        choices=["TRUE", "FALSE","True","False","T","F"],\
+    parser.add_argument("--inclu_vertical", default="False",\
+        choices=["TRUE", "FALSE","True","False","T","F","1","0"],\
         help="whether consider vertical bar or not")
     return parser.parse_args()
 
@@ -35,27 +35,43 @@ def main(root_path):
 
     # prepare for parameters
     args = parser_args()
-    thres = args.target
+    thres = args.thres
     profit_take = args.pt
     stop_loss = args.sl
     target = args.target
     hold_time = args.hold
-    is_vertical = args.is_vertical
+    if args.inclu_vertical in ["FALSE","False","F","0"]:
+        inclu_vertical = False
+    elif args.inclu_vertical in ["TRUE","True","T","1"]:
+        inclu_vertical = True
 
     CUMSUM_idx = filters.CUMSUM_filter(price, thres)
     N = len(CUMSUM_idx)
+    # each element in CUMSUM_idx is the time point when signal appears
+    # also because price is all close price, so we can only take action the next minute/day
+    # so label should start from CUMSUM_idx + 1
     events = pd.DataFrame({"start_t":CUMSUM_idx + 1, "end_t": CUMSUM_idx + hold_time,\
         "target_rtn":np.repeat(target,N), "side": np.repeat(0,N)})
-    meta_label = feature_mat.meta_label(price, events, profit_take, stop_loss, is_vertical)
+    meta_label = feature_mat.meta_label(price, events, profit_take, stop_loss, inclu_vertical)
     
-    Rmd = "Rscript gen_labelling.R --pt %f --sl %f --thres %d --target %f --hold %d --is_vertical %s"\
-     % (profit_take, stop_loss, thres, target, hold_time, is_vertical)
+    Rmd = "Rscript gen_labelling.R --pt %f --sl %f --thres %d --target %f --hold %d --inclu_vertical %s"\
+     % (profit_take, stop_loss, thres, target, hold_time, args.inclu_vertical)
     subprocess.check_output(Rmd, universal_newlines=True)
     meta_label_run = pd.read_csv(os.path.join(root_path,"meta_label_run.csv"))
-    meta_label_run = meta_label_run[["t0Fea","t1Fea","tLabel","ret","label"]]
-    res = meta_label_run.equals(meta_label)
+    meta_label_run = meta_label_run[["t0Fea","t1Fea","tLabel","label","ret"]]
+    # to compare index between python and R, we need to transform index in R minus 1
+    meta_label_run["t0Fea"] = meta_label_run["t0Fea"] - 1
+    meta_label_run["t1Fea"] = meta_label_run["t1Fea"] - 1
+    meta_label_run["tLabel"] = meta_label_run["tLabel"] - 1
+
+    feature_start_diff = len(np.where((meta_label["feature_start"]-meta_label_run["t0Fea"])!=0)[0])
+    feature_end_diff = len(np.where((meta_label["feature_end"]-meta_label_run["t1Fea"])!=0)[0])
+    label_idx_diff = len(np.where((meta_label["label_point"]-meta_label_run["tLabel"])!=0)[0])
+    rtn_diff = len(np.where((meta_label["return"]-meta_label_run["ret"]>1e-9))[0])
+    label_diff = len(np.where((meta_label["label"]-meta_label_run["label"])!=0)[0])
     
-    if res:
+    diff = [feature_start_diff,feature_end_diff,label_idx_diff,rtn_diff,label_diff]
+    if not np.any(diff):
         print("################################")
         print("#Awesome! your code is perfect!#")
         print("################################")
