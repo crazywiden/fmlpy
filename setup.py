b41fdc558a874ddaef67fd82cb9636720db02529
widen <widen1226@163.com>
2019-07-04 23:20:35 -0500

    add classifier

--- setup.py
+++ setup.py
@@ -2,20 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='fmlpy',
-    version='0.10dev',
+    version='0.1.0',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='package for Financial Machine Learning',
     long_description=open('README.md').read(),
     install_requires=['numpy','pandas'],
-    url='https://github.com/crazywiden/pyfml',
+    url='https://github.com/crazywiden/fmlpy',
     author='Xiuyu&Yuan',
     author_email='yuangao719@gmail.com',
     keywords = ['Machine Learning', 'Financial'], 
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Financial and Insurance Industry',  
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
     ]
 )
 
