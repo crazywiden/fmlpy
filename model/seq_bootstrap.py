0ba8c911ebc428b39b28420d6e3055c619c8ca42
widen <widen1226@163.com>
2019-07-04 21:25:32 -0500

    fixed little bugs

--- model/seq_bootstrap.py
+++ model/seq_bootstrap.py
@@ -3,7 +3,6 @@ import numpy as np
 
 
 def _occurance_matrix(label_start,label_end):
-<<<<<<< HEAD
     """
     generate a matrix shows which bar used info of data at which time
     e.g.
@@ -33,12 +32,13 @@ def _occurance_matrix(label_start,label_end):
     return matrix
 
 
-def seq_bootstrap(occ_matrix, n_classifier=1, n_sample=None, verbose=True):
+def seq_bootstrap(label_start, label_end, n_classifier=1, n_sample=None, verbose=True):
     """
     implement sequential bootstrap algorithm
     @parameters:
-    occ_matrix -- m by n matrix
-        output of _occurance_matrix()
+    label_start -- 1d vector
+    label_end -- 1d vector
+        note that label_start[i] < label_end[i] for all i
     n_classifier -- int
         how many weak classifiers needed to be trained
     n_sample -- int
@@ -52,6 +52,11 @@ def seq_bootstrap(occ_matrix, n_classifier=1, n_sample=None, verbose=True):
         m is number of classifiers
         n is number of samples
     """
+
+    occ_matrix = _occurance_matrix(label_start,label_end)
+    if not n_sample:
+        n_sample = occ_matrix.shape[0]
+
     result = []
     for i in range(n_classifier):
         first = np.random.randint(0, len(occ_matrix))
@@ -64,7 +69,7 @@ def seq_bootstrap(occ_matrix, n_classifier=1, n_sample=None, verbose=True):
             overlap = np.sum(overlap, axis=1)
             overlap_prob = overlap / np.sum(overlap)
 
-            choice = np.random.choice(range(len(occ_matrix)), prob=overlap_prob)
+            choice = np.random.choice(range(len(occ_matrix)), p=overlap_prob)
             sample.append(choice)
             benchmark += occ_matrix[choice]
 
@@ -72,73 +77,3 @@ def seq_bootstrap(occ_matrix, n_classifier=1, n_sample=None, verbose=True):
         if verbose:
             print('Round {}/{}: time elapsed -- {}'.format(i, n_classifier, time.time()-start_time))
     return result
-=======
-	"""
-	generate a matrix shows which bar used info of data at which time
-	e.g.
-	>>> label_start = [2,3,5]
-	>>> label_end = [6,7,12]
-	>>> _occurance_matrix(label_start, label_end)
-	[
-	[0,0,1,1,1,1,1,0,0,0,0,0,0],
-	[0,0,0,1,1,1,1,1,0,0,0,0,0],
-	[0,0,0,0,0,1,1,1,1,1,1,1,1]
-	]
-
-	@parameters:
-	label_start -- 1d vector
-	label_end -- 1d vector
-		note that label_start[i] < label_end[i] for all i
-	@returns:
-	occ_matrix -- m by n matrix
-		m is number of bars (len(label_start))
-		n is maximum time length (max(label_end))
-	"""
-	matrix = np.zeros(shape=(len(label_start), label_end[-1]))
-	i = 0
-	for s, e in zip(label_start, label_end):
-		matrix[i, s:e] = 1
-		i += 1
-	return matrix
-
-
-def seq_bootstrap(occ_matrix, n_classifier=1, n_sample=None, verbose=True):
-	"""
-	implement sequential bootstrap algorithm
-	@parameters:
-	occ_matrix -- m by n matrix
-		output of _occurance_matrix()
-	n_classifier -- int
-		how many weak classifiers needed to be trained
-	n_sample -- int
-		number of samples that need to draw
-		default should be number of bars, i.e. occ_matrix.shape[0]
-	verbose -- boolean
-		whether print time eclapse for each round
-		default is True
-	@returns:
-	sample_idx -- m by n matrix
-		m is number of classifiers
-		n is number of samples
-    """
-	result = []
-	for i in range(n_classifier):
-		first = np.random.randint(0, len(occ_matrix))
-		benchmark = occ_matrix[first]
-		sample = [first]
-
-		start_time = time.time()
-		for j in range(n_sample):
-			overlap = occ_matrix / (1+benchmark)
-			overlap = np.sum(overlap, axis=1)
-			overlap_prob = overlap / np.sum(overlap)
-
-			choice = np.random.choice(range(len(occ_matrix)), prob=overlap_prob)
-			sample.append(choice)
-			benchmark += occ_matrix[choice]
-
-		result.append(sample)
-		if verbose:
-			print('Round {}/{}: time elapsed -- {}'.format(i, n_classifier, time.time()-start_time))
-	return result
->>>>>>> 5ed43473297ec1df2af86ede75af746e334331b0
