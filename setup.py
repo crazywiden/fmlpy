1d32433d567f5cf1f63b6c5747176ef842eaf8e6
widen <widen1226@163.com>
2019-07-14 21:52:58 -0500

    change version number

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='fmlpy',
-    version='0.1.0',
-#     packages=find_packages(exclude=['tests*']),
-    packages=find_packages(),
+    version='0.1.1',
+    packages=find_packages(exclude=['tests*']),
     license='LICENSE',
     description='package for Financial Machine Learning',
     install_requires=['numpy','pandas'],
